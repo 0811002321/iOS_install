1d6f2d2321ce2f00dad298239f1a68326993eb9b
0811002321 <zhangkg1989@163.com>
2020-06-08 16:04:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,13 +15,9 @@ $json_string = file_get_contents('info.json');
  <title>酷我音乐iPad安装</title>
  </head>
  <body>
- <p>当前版本：$version($build) 更新时间：$time</p>
- <a href="https://172.17.89.237/ipad/server.crt">证书</a>(如果之前尚未安装过此证书，请先安装证书)<br/>
+ <p>当前版本：$version($build) 更新时间：$time</p><br/>
  <p>点击图片开始安装</p>
  <a href="itms-services://?action=download-manifest&url=https://172.17.89.237/ipad/manifest.plist"><img src="/ipad/icon.png" width="256" height="256"></a>
- <script type="text/javascript">
-	function install() {console.log("nidayede!!");}
- </script>
  </body>
  </html>
 EOT;
